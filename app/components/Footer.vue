0f0aee2d5f4a2358e72f6dd9315f24bf275e7086
lanseria <zhangchao564265135@hotmail.com>
2025-06-17 14:15:31 +0800

    fix(components): 更新 Footer 组件中的 GitHub 链接地址
    - 将 Footer 组件中的 GitHub 链接地址从 "https://github.com/lanseria/share-file-app" 修改为 "https://github.com/lanseria/LaunchDeck-nuxt-app"
    - 此修改确保用户点击 GitHub 图标时能够正确跳转到新的仓库地址

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div text="xl gray4" m-5 flex="~ gap3" justify-center>
     <NuxtLink i-carbon-campsite to="/" />
-    <a i-carbon-logo-github href="https://github.com/lanseria/share-file-app" target="_blank" />
+    <a i-carbon-logo-github href="https://github.com/lanseria/LaunchDeck-nuxt-app" target="_blank" />
     <a i-simple-icons-bilibili href="https://space.bilibili.com/8487409?spm_id_from=..0.0" target="_blank" />
     <DarkToggle />
   </div>
